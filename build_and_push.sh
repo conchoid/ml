1ea5b01633db9867b0f527be55a7827e13fa9ba6
Masato Oikawa <masato.oikawa@upstairs.jp>
2025-05-29 08:14:47 +0900

    Add curl retry

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-version="0.2.6"
+version="0.2.7"
 docker build . --tag "conchoid/ml:${version}" && docker push "conchoid/ml:${version}"
--- setup_ml.sh
+++ setup_ml.sh
@@ -24,7 +24,7 @@ setup_common() {
 setup_data_collection_env() {
   if ! type "go" > /dev/null; then
     go_version=1.11.13
-    curl -f -L -o go.tgz "https://dl.google.com/go/go${go_version}.linux-amd64.tar.gz" && \
+    curl $CURL_RETRY_OPT -f -L -o go.tgz "https://dl.google.com/go/go${go_version}.linux-amd64.tar.gz" && \
       tar -C /usr/local -xzf go.tgz && \
       rm go.tgz && \
       ln -s "/usr/local/go/bin/go" "/usr/local/bin/go"
@@ -36,7 +36,7 @@ setup_data_collection_env() {
 
 setup_xgboost() {
   # cmake (xgboost needs latest cmake)
-  curl -f -L -o "cmake.sh" "https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-Linux-x86_64.sh"
+  curl $CURL_RETRY_OPT -f -L -o "cmake.sh" "https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-Linux-x86_64.sh"
   mkdir -p /opt/cmake
   yes | sh cmake.sh --prefix=/opt/cmake
   ln -s /opt/cmake/cmake-3.18.4-Linux-x86_64/bin/cmake /usr/local/bin/cmake
